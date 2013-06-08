c2588ed22cfdb033fdf5aef9abc946069e5ded07
TJ Taylor <ttaylor@tendrilinc.com>
2013-06-08 12:42:46 -0600

    HexPoints can "travel" to another point in the grid.
    This isn't specifically traveling, but it allows me to get the points
    toward a specific direction from that hex

--- lib/catan/hex_point.rb
+++ lib/catan/hex_point.rb
@@ -1,5 +1,17 @@
+require 'matrix'
+
 module Catan
   class HexPoint
+    include Comparable
+    DIRECTIONS_IN_CUBE_COORDS = {
+      north: [0, 1, -1],
+      northeast: [1, 0, -1],
+      southeast: [1, -1, 0],
+      south: [0, -1, 1],
+      southwest: [-1, 0, 1],
+      northwest: [-1, 1, 0],
+    }
+
     attr_reader :x, :y
     def initialize(x, y)
       @x, @y = x, y
@@ -7,6 +19,23 @@ module Catan
 
     def z
       @z ||= 0 - x - y
+    end
+
+    def travel(direction)
+      directions = DIRECTIONS_IN_CUBE_COORDS[direction]
+      HexPoint.new x + directions[0], y + directions[1]
+    end
+
+    def <=>(other)
+      x <=> other.x && y <=> other.y
+    end
+
+    def hash
+      [x, y, z].hash
+    end
+
+    def eql?(other)
+      self == other
     end
   end
 end
--- spec/unit/catan/hex_point_spec.rb
+++ spec/unit/catan/hex_point_spec.rb
@@ -8,4 +8,18 @@ describe Catan::HexPoint do
     point = Catan::HexPoint.new 0, 0
     point.z.must_equal 0
   end
+
+  it 'can travel in a given direction' do
+    point = Catan::HexPoint.new 0, 0
+    new_point = point.travel(:northeast)
+    new_point.x.must_equal 1
+    new_point.y.must_equal 0
+    new_point.z.must_equal -1
+  end
+
+  it 'is equal if all the coords are the same' do
+    point = Catan::HexPoint.new 0, 0
+    other = Catan::HexPoint.new 0, 0
+    point.must_equal other
+  end
 end
